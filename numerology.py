aae7a60531e3225c5e836c24088bcf01d7207980
MERKON <merterenkonan62@gmail.com>
2019-06-28 14:18:46 +0300

    Update numerology.py

--- numerology.py
+++ numerology.py
@@ -7,9 +7,9 @@
 import sys
 
 def numerolog (string):
-    sum=0
-    two_sum = 0
-    main_sum=0
+    one_sum  = 0
+    two_sum  = 0
+    main_sum = 0
     # HARFLERİ TANIMLAMA
     alfabeno = [1,2,3,4,5,6,7,8,9,10,
                 11,12,13,14,15,16,17,18,19,
@@ -25,17 +25,17 @@ def numerolog (string):
     for a in range(len(array_string)):
         for b in range(len(alfabe)):
             if array_string[a] == buyuk_alfabe[b] or array_string[a] == alfabe[b]:
-               sum += alfabeno[b]
-    main_sum = sum
+               main_sum += alfabeno[b]
+    one_sum = main_sum
     # Sonuç Çift Rakamlıysa Tek Rakama İndirgeme
-    while len(list(str(sum))) > 1:
-        arr_sum = list(str(sum))
-        sum = 0
+    while len(list(str(one_sum))) > 1:
+        arr_sum = list(str(one_sum))
+        one_sum = 0
         for c in range(len(arr_sum)):
-            sum += int(arr_sum[c])
-        if len(list(str(sum))) == 2:
-            two_sum = sum
-    return sum,two_sum,main_sum;
+            one_sum += int(arr_sum[c])
+        if len(list(str(one_sum))) == 2:
+            two_sum = one_sum
+    return one_sum,two_sum,main_sum;
 
 numbers=numerolog(sys.argv[1:])
 print(list(numbers))
